1b2a6d1c71c3c90348fee5640d208babee34988d
pbomba <paul@paulbomba.com>
2018-10-29 13:20:28 -0400

    got the thing working

--- app/app.js
+++ app/app.js
@@ -48,37 +48,34 @@ const app = new App(config);
 app.setHandler({
     'LAUNCH': function() {
         this.ask(`Welcome to ${VotewithVoice}!` +
-                    ` Before we get started, please tell me what state ` +
-                    ` you're registered to vote in ` +
-                    ` Where are you flying to? `);
+                    ` To get started, ask me about a federal or state election in your ` +
+                    ` area, for example: who's running for governor in New York?` +
+                    `  `);
     },
-    'SearchFlightIntent': function(fromCity, toCity, date, ticketCount) {
+    'ContestInStateIntent': function(contest, state) {
         if (!this.alexaSkill().isDialogCompleted()) {
             this.alexaSkill().dialogDelegate();
-        } else if (!this.alexaSkill().hasSlotValue('ticketCount')) {
-            this.alexaSkill().dialogElicitSlot('ticketCount', 'How many tickets do you need?');
         } else if (this.alexaSkill().getIntentConfirmationStatus() !== 'CONFIRMED') {
             this.alexaSkill().dialogConfirmIntent(
-                'So you are flying from ' + fromCity.value +
-                ' to ' + toCity.value +
-                ' on ' + date.value +
-                ' and you need ' + ticketCount.value + ' tickets, right?'
+                'You want to know who is running for ' + contest.value +
+                ' in ' + state.value +
+                ' is that correct?'
             );
         } else if (this.alexaSkill().getIntentConfirmationStatus() === 'CONFIRMED') {
             let flightData = {
-                fromCity: fromCity.value,
-                toCity: toCity.value,
-                date: date.value,
-                ticketCount: ticketCount.value,
+                contest: contest.value,
+                state: state.value
             };
             this.toIntent('FlightApiIntent', flightData);
         }
     },
     'FlightApiIntent': function(flightData) {
-        console.log(flightData);
+        // console.log(flightData);
         this.tell('Your flight is booked, thanks!');
     },
 });
 
 /** 'app' is exported for use as an entrypoint in index: */
 module.exports.app = app;
+
+
